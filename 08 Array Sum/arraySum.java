58e546c46273c45086aa8dcf5769ba37c86ee1e2
imharish18 <iharish0018@gmail.com>
2025-05-16 12:53:28 +0530

    Another Problem Solved

---
+++ 08 Array Sum/arraySum.java
@@ -0,0 +1,14 @@
+public class app {
+    public static int functionSum(int[] arr) {
+        int sum = 0;
+        for(int i=0;i<arr.length; i++){
+            sum+=arr[i];
+        }
+        System.out.println(sum);
+        return sum;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        functionSum(arr);
+    }
+}
