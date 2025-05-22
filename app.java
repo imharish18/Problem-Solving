f75a57bc66e34806083c97c81ecf0454078a4e4d
imharish18 <iharish0018@gmail.com>
2025-05-22 12:38:34 +0530

    Sqrt

--- app.java
+++ app.java
@@ -1,18 +1,16 @@
-class app {
-    public static int peakIndexInMountainArray(int[] arr) {
-        int n = arr.length;
-        int lo=1, hi=n-2;
+class mountainProblem {
+    public static int mySqrt(int x) {
+        int lo = 0, hi=x;
+        int mid=0;
         while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]) return mid;
-            else if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1]) lo=mid+1;
-            else if(arr[mid]<arr[mid-1] && arr[mid]>arr[mid+1]) hi=mid-1;
+            mid = lo + (hi-lo)/2;
+            if(mid*mid==x) return mid;
+            else if(mid*mid>x) hi = mid-1;
+            else lo = mid+1;
         }
-        return -1;
+        return hi;
     }
     public static void main(String[] args){
-        int[] ar = {1,2,3,4,5,3,2};
-        int x = peakIndexInMountainArray(ar);
-        System.out.println(x);
+    System.out.println(mySqrt(14));
     }
 }
