553c2affd89aa576b647e6a30e0bcc83c9f411f6
imharish18 <iharish0018@gmail.com>
2025-05-24 11:07:14 +0530

    new solved

--- 52 Power of a With Reccurence/powab.java
+++ 52 Power of a With Reccurence/powab.java
@@ -1,9 +1,6 @@
-class app{
+public class powab {
     public static int pow(int a, int b) {
         if(b==0) return 1;
         return a*pow(a,b-1);
     }
-    public static void main(String[] args){
-        System.out.println(pow(2,10));
-    }
-} 
+}
