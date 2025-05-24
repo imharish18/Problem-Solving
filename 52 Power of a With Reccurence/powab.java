0872f06aab767d58bd6283e2da3ef57a90fa12ee
imharish18 <iharish0018@gmail.com>
2025-05-24 11:21:38 +0530

    doing optimization

--- 52 Power of a With Reccurence/powab.java
+++ 52 Power of a With Reccurence/powab.java
@@ -3,4 +3,16 @@ public class powab {
         if(b==0) return 1;
         return a*pow(a,b-1);
     }
+        public static int powLogarithimic(int a, int b) {
+        if(b==0) return 1;
+        else if(b==1) return a;
+        else if(b%2==0){
+            int ans = pow(a,b/2);
+            return ans*ans;
+        }else{
+            int ans = pow(a,b/2);
+            return ans*ans*a;
+        }
+
+    }
 }
--- app.java
+++ app.java
@@ -1,9 +1,17 @@
 class app{
     public static int pow(int a, int b) {
         if(b==0) return 1;
-        return a*pow(a,b-1);
+        else if(b==1) return a;
+        else if(b%2==0){
+            int ans = pow(a,b/2);
+            return ans*ans;
+        }else{
+            int ans = pow(a,b/2);
+            return ans*ans*a;
+        }
+
     }
     public static void main(String[] args){
-        System.out.println(pow(2,10));
+        System.out.println(pow(2,7));
     }
 } 
