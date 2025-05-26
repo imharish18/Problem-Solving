c2d2cce8672a28828ebd6ef1c8865dc50ceaf5f9
imharish18 <iharish0018@gmail.com>
2025-05-26 09:50:41 +0530

    solved another

--- 60 Subsets Increasing from the 1/sbsetes.java
+++ 60 Subsets Increasing from the 1/sbsetes.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
-
-class app{
+import java.util.*;
+public class sbsetes{
     public static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         recursion(nums, 0, result, new ArrayList<>());
@@ -10,8 +8,8 @@ class app{
         return result;
     }
     public static void recursion(int[] nums, int idx, List<List<Integer>> result, List<Integer> ans){
-        if(idx == nums.length || ans.size()==4){
-            if(ans.size()==4 && ans.get(0)==1) result.add(new ArrayList<>(ans));
+        if(idx == nums.length){
+            result.add(new ArrayList<>(ans));
             return;
         }
         recursion(nums, idx+1, result, ans);
@@ -19,9 +17,5 @@ class app{
         recursion(nums, idx+1, result, ans);
         ans.remove(ans.size()-1);
     }
-    
-    public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6};
-        subsets(arr);
-    }
-} 
+
+}
