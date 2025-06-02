bce0aa35f6cd0be396ebe3bf0f62e82d1c3b5dd4
imharish18 <iharish0018@gmail.com>
2025-06-02 21:31:59 +0530

    continued

---
+++ 80 Creation of LinkedLIst/crll.java
@@ -0,0 +1,29 @@
+public class crll {
+    class Node{
+        int val;
+        Node next;
+        Node(int val){
+            this.val = val;
+        }
+    }
+    public class trike{
+        public static void main(String[] args) {
+            Node a = new Node(10);
+            Node b = new Node(20);
+            Node c = new Node(30);
+            Node d = new Node(40);
+            Node e = new Node(50);
+            Node f = new Node(60);
+            a.next = b;
+            a.next.next = c;
+            a.next.next.next = d;
+            a.next.next.next.next = a;
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(d);
+            System.out.println(e);
+            System.out.println(f);
+        }
+    }
+}
--- trike.java
+++ trike.java
@@ -1,11 +1,30 @@
 import java.util.*;
-public class trike{
-
-
-    public static String longestPalindrome(String s) {
-        
+class Node{
+    int val;
+    Node next;
+    Node(int val){
+        this.val = val;
     }
+
+}
+public class trike{
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("aaabb")); // Note the L at the end
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+        Node f = new Node(60);
+        a.next = b;
+        a.next.next = c;
+        a.next.next.next = d;
+        a.next.next.next.next = a;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
     }
 }
+
