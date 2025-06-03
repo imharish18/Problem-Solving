21f5d1bae71dd8267b1d42a3b2e3eca02cd0be3f
imharish18 <iharish0018@gmail.com>
2025-06-03 11:14:13 +0530

    I am not done yet

---
+++ 84 LL Display and Insertion/insDis84.java
@@ -0,0 +1,24 @@
+class insDis84{
+    Node head;
+    Node tail;
+    int size=0;
+
+    public void insertAtEnd(int val){
+        Node temp = new Node(val);
+        if(head==null) head = tail = temp;
+        else{
+            tail.next = temp;
+            tail = temp;
+        }
+        size++;
+    }
+    public void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.val+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
+
--- Iimch.java
+++ Iimch.java
@@ -7,28 +7,43 @@ class Node{
     }
 }
 
-public class Iimch{
-    public static void printLinked(Node head){
-        if (head==null) return;
-        System.out.println(head.val);
-        printLinked(head.next);
+class SLL{
+    Node head;
+    Node tail;
+    int size=0;
+
+    public void insertAtEnd(int val){
+        Node temp = new Node(val);
+        if(head==null) head = tail = temp;
+        else{
+            tail.next = temp;
+            tail = temp;
+        }
+        size++;
     }
+    public void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.val+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
+public class Iimch{
     public static void main(String[] args) {
-        Node a = new Node(10);
-        Node b = new Node(20);
-        Node c = new Node(30);
-        Node d = new Node(40);
-        Node e = new Node(50);
-        Node f = new Node(60);
-
-        a.next = b;
-        b.next = c;
-        c.next = d;
-        d.next = e;
-        e.next = f;
-
-        Node head = a;
-        printLinked(head);
+        SLL list = new SLL();
+        list.insertAtEnd(5);
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+        list.display();
+        System.out.println(list.size);
+        list.insertAtEnd(30);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+        list.display();
+        System.out.println(list.size);
     }
 }
 
