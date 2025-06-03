5a24ba2d90141c7bde318a31e72c115716bd7c7f
imharish18 <iharish0018@gmail.com>
2025-06-03 13:10:59 +0530

    Leetcode

---
+++ 90 Return the middle of the LinkedList/iam90.java
@@ -0,0 +1,27 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val){ 
+        this.val = val; 
+    }
+    ListNode(int val, ListNode next){ 
+        this.val = val; this.next = next; 
+    }
+}
+public class iam90 {
+    public ListNode middleNode(ListNode head) {
+        ListNode temp = head;
+        int len=0;
+        while(temp!=null){
+            temp=temp.next;
+            len++;
+        }
+        len=len/2+1;
+        temp = head;
+        for(int i=0; i<len-1; i++){
+            temp=temp.next;
+        }
+        return temp;
+    }
+}
