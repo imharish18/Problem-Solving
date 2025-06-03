c65c6ff45fc8f5db239acffdb54e48697efe9024
imharish18 <iharish0018@gmail.com>
2025-06-03 11:20:16 +0530

    I am not done yet

--- 85 Inserting at head method of LL/iam85.java
+++ 85 Inserting at head method of LL/iam85.java
@@ -1,10 +1,24 @@
 public class iam85 {
-        public void insertAtStart(int val){
+    Node head;
+    Node tail;
+    int sz=0;
+
+    public void insertAtStart(int val){
         Node temp = new Node(val);
         if(head==null) head = tail = temp;
         else{
             temp.next = head;
             head = temp;
+        }
+        sz++;
+    }
+
+    public void insertAtEnd(int val){
+        Node temp = new Node(val);
+        if(head==null) head = tail = temp;
+        else{
+            tail.next = temp;
+            tail = temp;
         }
         sz++;
     }
