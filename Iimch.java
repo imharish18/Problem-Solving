c05abc4304a47228ccf91d4bc2005183a16ad234
imharish18 <iharish0018@gmail.com>
2025-06-07 15:42:30 +0530

    DONE WITH DLL

--- Iimch.java
+++ Iimch.java
@@ -9,7 +9,6 @@ class Node{
         this.val = val;
     }
 }
-
 class SLL{
     Node head;
     Node tail;
@@ -248,120 +247,201 @@ class SLL{
         }
     }
 }
-public class Iimch{
-    public static void displayW(Node head){
+
+class dNode{
+    int val;
+    dNode next;
+    dNode prev;
+    dNode(int val){
+        this.val = val;
+    }
+}
+class DLL{
+    dNode head;
+    dNode tail;
+    int size;
+
+    public void display(){
+        dNode temp = head;
+        while(temp!=null){
+            System.out.print(temp.val+" ");
+            temp = temp.next;
+        }System.out.println();
+    }
+
+    public void insertAtTail(int val){
+        dNode temp = new dNode(val);
+        if(size==0) head = tail = temp;
+        else{
+            tail.next = temp;
+            temp.prev = tail;
+            tail = temp;
+        }
         
-        Node slow = head;
-        while(slow!=null){
-            System.out.print(slow.val+" ");
-            slow=slow.next;
+        size++;
+    }
+    public void insertAtHead(int val){
+        dNode temp = new dNode(val);
+        if(size==0) head = tail = temp;
+        else{
+            temp.next=head;
+            head.prev = temp;
+            head = temp;
+        }
+    }
+    public void insertAt(int val, int idx) {
+        if(idx==0){
+            insertAtHead(val);
+            return;
+        }
+        if(idx==size){
+            insertAtTail(val);
+            return;
+        }
+        if(idx>size){
+            System.out.println("OUT OF BOUND");
+            return;
+        }
+        dNode temp = new dNode(val);
+        dNode x = head;
+        for(int i=1; i<idx; i++){
+            x = x.next;
+        }
+        dNode y = x.next;
+        x.next = temp;
+        y.prev = temp;
+        temp.next = y;
+        temp.prev = x;
+        size++;
+    }
+
+    public void deleteAtHead() throws Error{
+        if(size==0) throw new Error("List is Empty");
+        head=head.next;
+        head.prev = null;
+        size--;
+    }
+    public void deleteAtTail() throws Error{
+        if(size==0) throw new Error("List is Empty");
+        tail=tail.prev;
+        tail.next = null;
+        size--;
+    }
+    public void deleteAt(int idx){
+        if(idx==0){
+            deleteAtHead();
+            return;
+        }
+        if(idx==size-1){
+            deleteAtTail();
+            return;
+        }
+        dNode temp = head;
+        for(int i=1; i<idx; i++){
+            temp=temp.next;
+        }
+        temp.next = temp.next.next;
+        temp=temp.next;
+        temp.prev = temp.prev.prev;
+        System.out.println(temp.val);
+    }
+
+    public void getHead(){
+        if(head==null) System.out.println("Empty List");
+        System.out.println(head.val);
+    }
+    public void getTail(){
+        if(size==0) System.out.println("Empty List");
+        System.out.println(tail.val);
+    }
+    public void getAt(int idx) throws Error{
+        if(idx>=size) throw new Error("OUT OF BOUND");
+        if(idx==0){
+            getHead();
+            return;
+        }
+        if(idx==size-1){
+            getTail();
+            return;
+        }
+        dNode temp = head;
+        for(int i=0; i<idx; i++){
+            temp=temp.next;
+        }
+        System.out.println(temp.val);
+    }
+    public void setHead(int newVal){
+        if(head==null) System.out.println("Empty List");
+        head.val=newVal;
+    }
+    public void setTail(int newVal){
+        if(size==0) System.out.println("Empty List");
+        tail.val=newVal;
+    }
+    public void setAt(int idx, int newVal) throws Error{
+        if(idx>=size) throw new Error("OUT OF BOUND");
+        if(idx==0){
+            setHead(newVal);
+            return;
+        }
+        if(idx==size-1){
+            setTail(newVal);
+            return;
+        }
+        dNode temp = head;
+        for(int i=0; i<idx; i++){
+            temp=temp.next;
+        }
+        temp.val=newVal;
+    }
+
+}
+
+public class Iimch{
+    public static void display(dNode head){
+        dNode temp = head;
+        while(temp!=null){
+            System.out.print(temp.val+" ");
+            temp = temp.next;
         }
         System.out.println();
     }
-    public static Node[] splitListToParts(Node head, int k) {
-        ArrayList<Node> arr = new ArrayList<>();
-        int n = 0;
-        Node temp = head;
+    public static void displayReverse(dNode tail){
+        dNode temp = tail;
         while(temp!=null){
+            System.out.print(temp.val+" ");
+            temp = temp.prev;
+        }
+        System.out.println();
+    }
+    public static void displayWithRandomNode(dNode node){
+        dNode temp = node;
+        while(temp.prev!=null){
+            temp = temp.prev;
+        }
+        while(temp!=null){
+            System.out.print(temp.val+" ");
             temp = temp.next;
-            n++;
         }
-
-        int size = n/k;
-        int extra = n%k;
-        temp = head;
-        int len=1;
-
-        while(temp!=null){
-            int s=size;
-            if(extra>0) s++;
-            if(len==1) arr.add(temp);
-            if(len==s){
-                Node a = temp.next;
-                temp.next=null;
-                temp=a;
-                len=1;
-                extra--;
-            }else{
-                temp=temp.next;
-                len++;
-            }
-        }
-        for(int i=0; i<arr.size(); i++){
-            displayW(arr.get(i));
-        }
-    return arr.toArray(new Node[k]);
+        System.out.println();
     }
-    public static Node reverse(Node head){
-        if (head == null || head.next == null) return head; 
-        Node prev = null;
-        Node curr = head;
-
-        while (curr != null) {
-            Node next = curr.next;
-            curr.next = prev;
-            prev = curr;        
-            curr = next;          
-        }
-        return prev;
-    }
-    public static void reverseKGroup(Node head, int k) {
-        if(k==1) return;
-        Node dummy = new Node(-1);
-
-        Node d = dummy;
-        Node back = head;
-        Node temp = head;
-        Node front = head;
-        int len = 1;
-        while(front!=null){
-            if(len<k){
-                front = front.next;
-                len++;
-            }else{
-                len=1;
-                temp=front;
-                front=front.next;
-                temp.next=null;
-                reverse(back);
-                d.next=temp;
-                d=back;
-                back.next=front;
-                back=back.next;
-            }
-        }
-        displayW(dummy.next);
-       
-    }
-
-
-    public static void nodesBetweenCriticalPoints(Node head) {
-        Node left = head, mid = head.next, right = head.next.next;
-        int first = -1, last = -1, idx = 1;
-        while(right!=null){
-            if(mid.val<left.val && mid.val<right.val || mid.val>left.val && mid.val>right.val){
-                if(first==-1) first = idx;
-                last = idx;
-
-            }
-            idx++;
-            left= left.next;
-            mid = mid.next;
-            right = right.next;
-        }
-        if(first==last) return;
-    }
-
     public static void main(String[] args) {
-        SLL l1 = new SLL();
-        l1.insertAtEnd(5);
-        l1.insertAtEnd(2);
-        l1.insertAtEnd(3);
-        l1.insertAtEnd(4);
-        l1.insertAtEnd(5);
-        l1.display();
-        nodesBetweenCriticalPoints(l1.head);
-        l1.display();
+        DLL list = new DLL();
+        list.insertAtTail(10);
+        list.insertAtTail(20);
+        list.insertAtTail(40);
+        list.insertAtTail(50);
+        list.insertAt(30,2);
+        list.display();
+        list.setHead(1);
+        list.setTail(5);
+        list.setAt(1,2);
+        list.setAt(2,3);
+        list.setAt(3,4);
+        list.display();
+
+        System.out.println(list.size);
+
     }
 }
 
