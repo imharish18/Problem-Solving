a5c92e35e0cef0d0244fc7b85a9be0f3e4d62ca4
imharish18 <iharish0018@gmail.com>
2025-06-11 14:02:49 +0530

    I am 121

---
+++ 121 Maximum Cosecutive Ones/iam121.java
@@ -0,0 +1,25 @@
+public class iam121 {
+    public int longestOnes(int[] nums, int k) {
+        int n = nums.length, maxLen = Integer.MIN_VALUE, flips = 0, i=0,j=0;
+        if(n==k) return n;
+        while(j<n){
+            if(nums[j]==1) j++;
+            else{
+                if(flips<k) {
+                    flips++;
+                    j++;
+                }
+                else{
+                    maxLen = Math.max(maxLen,j-i);
+                    while(nums[i]==1) i++;
+                    i++;
+                    j++;
+                }
+            }
+            
+        }
+        maxLen = Math.max(maxLen,j-i);
+    return maxLen;
+    }
+}
+
--- power.java
+++ power.java
@@ -1,27 +1,45 @@
 import java.util.*;
 
 class power{
-    public static void negNo(int[] ar, int target) {
+    public static void negNo(int[] ar, int k) {
         int n = ar.length;
-        int i=0, j=0;
-        int len = 0;
-        int maxLen = n;
-        int sum = 0;
+        int i = k, j = k;
+
+        int p = 0;
+        int max = 0;
+
+        int[] ans = new int[n];
+
+        for(int x=1; x<k; x++){
+            if(ar[x]>ar[max]) max = x;
+        }
+        for(int x=0; x<max; x++){
+            ans[p]=ar[max];
+            p++;
+        }
+
         while(j<n){
-            sum+=ar[j];
-            while(sum>=target){
-                len=j-i+1;
-                maxLen = Math.min(maxLen, len);
-                sum-=ar[i];
-                i++;
+            if(ar[j]<ar[max]){
+                ans[p]=ar[max];
+                p++;
+                System.out.println(ar[j]+" and " +ar[max]);
+            }else{
+                System.out.println(ar[j]+" and " +ar[max]);
+                max=j;
+                ans[p] = ar[max];
+                p++;
             }
             j++;
+            i++;
         }
-        System.out.println(maxLen);
+        for(int a : ans){
+            System.out.print(a+" ");
+        }
+
     }
     public static void main(String[] args) {
-        int[] nums = {1,2,4,6,3,4,3};
-        negNo(nums, 7);
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        negNo(nums, 3);
 
     }
 }
