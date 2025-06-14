54c0e7dd454e16893e2ed359d34e669a0393a56e
imharish18 <iharish0018@gmail.com>
2025-06-14 08:27:33 +0530

    reversing the Q

--- power.java
+++ power.java
@@ -1,47 +1,25 @@
 import java.util.*;
 import java.util.Stack;
 public class power {
-    public static void main(String[] args) {
-        Map<Character, Integer> priority = new HashMap<>();
-        priority.put('+', 1);
-        priority.put('-', 1);
-        priority.put('*', 2);
-        priority.put('/', 2);
-        priority.put('%', 2);  
+     public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
 
-        Stack<String> val = new Stack<>();
-        Stack<Character> op = new Stack<>();
+        Queue q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
 
-        String s = "(7+9)*4/8-3";
-        int n=s.length();
-        for(int i=0; i<n; i++){
-            if(Character.isDigit(s.charAt(i))) val.push((s.charAt(i)-48)+"");
-            else{
-                if(op.isEmpty() || priority.get(s.charAt(i))>priority.get(op.peek())) op.push(s.charAt(i));  
-                else{
-                    while(!op.isEmpty() && priority.get(s.charAt(i))<=priority.get(op.peek())){
-                        String val2 = val.pop();
-                        String val1 = val.pop();
-                        char ch = op.pop();
-                        String ans = ch+val1+val2;
-                        val.push(ans);
-                    }
-                    op.push(s.charAt(i));
-                }
+        while(!q.isEmpty()) st.push((int)q.remove());
+        System.out.println(st);
+        System.out.println(q);
+        while (!st.isEmpty()) q.add(st.pop());
+            
+        System.out.println(st);
+        System.out.println(q);
 
-            }
 
-        }
-        while(!op.isEmpty()){
-            String val2 = val.pop();
-            String val1 = val.pop();
-            char ch = op.pop();
-            String ans = ch+val1+val2;
-            val.push(ans);
-
-        }
-        System.out.println(val);
-        System.out.println(op);
-        System.out.println(val.peek());
+        System.out.println(q);
     }
 }
