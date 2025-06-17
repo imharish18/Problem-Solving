7ba0c68a558d853f1c460f2e69f85484cd9427ec
imharish18 <iharish0018@gmail.com>
2025-06-17 16:07:49 +0530

    I am 144

--- 144 Iterative Traversals of the Tree/iam144GOD.java
+++ 144 Iterative Traversals of the Tree/iam144GOD.java
@@ -1,21 +1,6 @@
-import java.util.ArrayDeque;
 import java.util.Stack;
-import java.util.List;
-import java.util.Queue;
-
-class TreeNode{
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int val){
-        this.val=val;
-        this.left=null;
-        this.right=null;
-    }
-}
-
-public class power {
-    public static void preOrderTraversal(TreeNode root){
+public class iam144GOD {
+        public static void preOrderTraversal(TreeNode root){
         Stack<TreeNode> st = new Stack();
         if(root!=null) st.push(root);
         while(!st.isEmpty()){
@@ -58,26 +43,5 @@ public class power {
         while (!ans.isEmpty()) {
             System.out.print(ans.pop()+" ");
         }
-    }
-
-
-    public static void main(String[] args) {
-        TreeNode a = new TreeNode(1);
-        TreeNode b = new TreeNode(2); 
-        TreeNode c = new TreeNode(3);
-        TreeNode d = new TreeNode(4);
-        TreeNode e = new TreeNode(5);
-        TreeNode f = new TreeNode(6);
-        TreeNode g = new TreeNode(7);
-        a.left=b; a.right = c;
-        b.left = d; b.right=e;
-        c.left=f; c.right=g;
-
-        preOrderTraversal(a);
-        System.out.println();
-        postOrderTraversal(a);
-        System.out.println();
-        inOrderTraversal(a);
-
     }
 }
