9cb42ad02431488e677d9b2d5f29230aee89acce
imharish18 <iharish0018@gmail.com>
2025-06-21 10:36:03 +0530

    Iam152

---
+++ 154 Inserting the Element in the BST/iam154.java
@@ -0,0 +1,8 @@
+public class iam154 {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        if (val < root.val) root.left = insertIntoBST(root.left, val); 
+        else if (val > root.val) root.right = insertIntoBST(root.right, val);
+        return root;
+    }
+}
