62a45ec2e228a8b714911bb36638f486186a4f9c
imharish18 <iharish0018@gmail.com>
2025-06-24 11:21:53 +0530

    Done new

--- power.java
+++ power.java
@@ -3,7 +3,11 @@ import java.util.*;
 public class power {
 
     public static void main(String[] args) {
+        HashMap<String,Integer> map = new HashMap<>();
+        map.put("Harish", 1);
+        map.put("Raghu", 44);
 
+        System.out.println(map.keySet());
 
     }
 }
