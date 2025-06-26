23e032e9da491451ceaaeb2f5b7e4df436bf42e0
imharish18 <iharish0018@gmail.com>
2025-06-26 09:42:05 +0530

    Done new

--- 165 Deep Copy of LL/iam165.java
+++ 165 Deep Copy of LL/iam165.java
@@ -1,31 +1,31 @@
 import java.util.*;
 public class iam165 {
-        public Node deepCopy(Node head){
-        Node head2 = new Node(head.val);
-        Node t1 = head.next;
-        Node t2 = head2;
+        public Nodee22 deepCopy(Nodee22 head){
+        Nodee22 head2 = new Nodee22(head.val);
+        Nodee22 t1 = head.next;
+        Nodee22 t2 = head2;
         while(t1!=null){
-            Node temp = new Node(t1.val);
+            Nodee22 temp = new Nodee22(t1.val);
             t2.next = temp;
             t2 = t2.next;
             t1 = t1.next;
         }
         return head2;
     }
-    public Node copyRandomList(Node head) {
+    public Nodee22 copyRandomList(Nodee22 head) {
         if(head==null) return null;
-        HashMap<Node,Node> map = new HashMap<>();
-        Node ans = deepCopy(head);
-        Node t1 = head;
-        Node t2 = ans;
+        HashMap<Nodee22,Nodee22> map = new HashMap<>();
+        Nodee22 ans = deepCopy(head);
+        Nodee22 t1 = head;
+        Nodee22 t2 = ans;
 
         while(t1!=null){
             map.put(t1,t2);
             t1=t1.next;
             t2=t2.next;
         }
-        for(Node ori : map.keySet()){
-            Node dup = map.get(ori);
+        for(Nodee22 ori : map.keySet()){
+            Nodee22 dup = map.get(ori);
            // if(ori.random!=null) dup.random = map.get(ori.random);
         }
         return ans;
