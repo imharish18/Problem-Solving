dd2a8bee322eafbc6c3d17f8e9d8b671e4bb8259
imharish18 <iharish0018@gmail.com>
2025-06-26 09:41:36 +0530

    Done new

--- 163 TOP VIEW Hackerrank.java/hackerank.java
+++ 163 TOP VIEW Hackerrank.java/hackerank.java
@@ -1,14 +1,14 @@
 import java.util.*;
 import java.io.*;
 
-class Node {
+class Nodee22 {
     public static final int val = 0;
-    public static Node next = null;
-    Node left;
-    Node right;
+    public static Nodee22 next = null;
+    Nodee22 left;
+    Nodee22 right;
     int data;
     
-    Node(int data) {
+    Nodee22(int data) {
         this.data = data;
         left = null;
         right = null;
@@ -19,27 +19,27 @@ class hackerank {
 
 	/* 
     
-    class Node 
+    class Nodee22 
     	int data;
-    	Node left;
-    	Node right;
+    	Nodee22 left;
+    	Nodee22 right;
 	*/
     public static class Pair{
-        Node node;
+        Nodee22 node;
         int level;
-        Pair(Node node, int level){
+        Pair(Nodee22 node, int level){
             this.node = node;
             this.level = level;
         }
     }
-	public static void topView(Node root) {
+	public static void topView(Nodee22 root) {
       HashMap<Integer,Integer> map = new HashMap<>();
       Queue<Pair> q = new LinkedList<>();
       q.add(new Pair(root, 0));
       int minLevel = Integer.MAX_VALUE, maxLevel = Integer.MIN_VALUE;
       while(!q.isEmpty()){
         Pair temp = q.remove();
-        Node node = temp.node;
+        Nodee22 node = temp.node;
         int level = temp.level;
         minLevel = Math.min(minLevel, level);
         maxLevel = Math.max(maxLevel, level);
@@ -55,11 +55,11 @@ class hackerank {
       
     }
 
-	public static Node insert(Node root, int data) {
+	public static Nodee22 insert(Nodee22 root, int data) {
         if(root == null) {
-            return new Node(data);
+            return new Nodee22(data);
         } else {
-            Node cur;
+            Nodee22 cur;
             if(data <= root.data) {
                 cur = insert(root.left, data);
                 root.left = cur;
@@ -74,7 +74,7 @@ class hackerank {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
-        Node root = null;
+        Nodee22 root = null;
         while(t-- > 0) {
             int data = scan.nextInt();
             root = insert(root, data);
--- power.java
+++ power.java
@@ -1,24 +1,17 @@
 import java.util.*;
 
 public class power {
-
-    public static int maxOperations(int[] nums, int k) {
-        int count = 0;
-
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int x : nums){
-            map.put(x,map.getOrDefault(x,0)+1);
-        }
-        for(int x : map.keySet()){
-            if(map.get(x)==1) map.remove(x);
-            else map.put(x,map.getOrDefault(x,0)-1);
-            if(map.containsKey(k-x) && map.get(k-x)>0){
-                count++;
-            map.put(x,map.getOrDefault(x,0)-1);
-            }
-        }
-        return count;
-    }
     public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(5);
+        pq.add(10);
+        pq.add(1);
+        pq.add(3);
+        
+        System.out.println(pq);
+
+
+
+
     }
 }
