990afc90aa09edaec88d017b79592186949f6e5d
imharish18 <iharish0018@gmail.com>
2025-06-27 12:04:02 +0530

    done new

--- heapImplementationsByArray.java
+++ heapImplementationsByArray.java
@@ -12,7 +12,7 @@ class minHeap{
     public void upheapify(int idx){
         if(idx==0) return;
         int p = (idx-1)/2;
-        if(ar[p]<ar[idx]){
+        if(ar[p]>ar[idx]){
             swap(p,idx);
             upheapify(p);
         }
@@ -25,11 +25,23 @@ class minHeap{
     public int size(){
         return size;
     }
+    public int peek(){
+        if(size==0){
+        System.out.println("Heap is EMPTY");
+        return -1;
+        }
+        return ar[0];
+    }
     
 }
 public class heapImplementationsByArray {
     public static void main(String[] args) {
         minHeap pq = new minHeap(5);
-
+        pq.add(5);
+        pq.add(0);
+        pq.add(6);
+        pq.add(4);
+        pq.add(2);
+        System.out.println(pq.peek());
     }
 }
