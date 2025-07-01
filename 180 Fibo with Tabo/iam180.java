3795f881a3cff86883ed4244491170a6fbb3f059
imharish18 <iharish0018@gmail.com>
2025-07-01 10:53:49 +0530

    i am here now:

--- 180 Fibo with Tabo/iam180.java
+++ 180 Fibo with Tabo/iam180.java
@@ -1,4 +1,20 @@
 public class iam180 {
+    static int[] dp;
+    public int fibo(int n) {
+        if(n<2) return n;
+        if(dp[n]!=0) return dp[n];
+        int ans = fibo(n-1)+fibo(n-2);
+        dp[n]=ans;
+        return ans;
+    }
+    public int fibMemoization(int n) {
+        dp = new int[n+1];
+        return fibo(n);
+    }
+
+
+//Tabulation
+
     public int fib(int n) {
         if(n<2) return n;
         int[] ar = new int[n+1];
