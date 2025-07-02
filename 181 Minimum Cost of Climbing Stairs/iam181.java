6a76ccdf6011c7d521ba5793bf3bb3f68fb3810a
imharish18 <iharish0018@gmail.com>
2025-07-02 11:04:24 +0530

    I am ready with this now

--- 181 Minimum Cost of Climbing Stairs/iam181.java
+++ 181 Minimum Cost of Climbing Stairs/iam181.java
@@ -1,5 +1,15 @@
 import java.util.*;
 public class iam181 {
+    public int minCostClimbingStairs2(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i=2; i<n; i++) dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
+        return Math.min(dp[n-1],dp[n-2]);
+    }
+
+    // Recursion + Memoization
     public int minCost(int[] cost, int i, int[] dp){
         if(i==0 || i==1) return cost[i];
         if(dp[i]!=-1) return dp[i];
