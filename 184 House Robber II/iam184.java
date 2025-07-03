67da0a3972d353a9ff2a6005c252f3cb96887c84
imharish18 <iharish0018@gmail.com>
2025-07-03 06:51:22 +0530

    i am here

--- 184 House Robber II/iam184.java
+++ 184 House Robber II/iam184.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class iam184 {
     public int roblinear(int[] nums, int i, int n){
         int[] dp = new int[n+1];
@@ -13,6 +14,25 @@ public class iam184 {
         if(n==2) return Math.max(nums[0],nums[1]);
         return Math.max(roblinear(nums,0,n-2),roblinear(nums,1,n-1));
         
+    }
+    // Recursion
+    public int robRecursive(int[] nums, int i, int n, int[] dp){
+        if(i>n) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int rob = nums[i]+robRecursive(nums,i+2,n,dp);
+        int skip = robRecursive(nums,i+1,n,dp);
+        return dp[i] =  Math.max(rob,skip);
+    }
+    public int rob2(int[] nums) {
+        int n = nums.length;        
+        if(n<=1) return nums[0];
+        if(n==2) return Math.max(nums[0],nums[1]);
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        int case1 = robRecursive(nums, 0, n - 2, dp);
+        Arrays.fill(dp, -1);
+        int case2 = robRecursive(nums, 1, n - 1, dp);
+        return Math.max(case1,case2);
     }
 }
 
